104584ba5fe3fc42dfaf839c45eddfec8ee08de9
radhouene101 <radhouene.berrzig@esprit.tn>
2024-04-23 11:36:33 +0100

    initial

---
+++ src/main/java/test/maryem/springtemplateproject/SpringTemplateProjectApplication.java
@@ -0,0 +1,13 @@
+package test.maryem.springtemplateproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTemplateProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTemplateProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/maryem/springtemplateproject/SpringTemplateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package test.maryem.springtemplateproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTemplateProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
