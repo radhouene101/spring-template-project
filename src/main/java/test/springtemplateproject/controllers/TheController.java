74b9e531c354f4645cf815a2aab93848c0f30a34
radhouene101 <radhouene.berrzig@esprit.tn>
2024-04-29 23:14:42 +0100

    fixes

--- src/main/java/test/springtemplateproject/controllers/TheController.java
+++ src/main/java/test/springtemplateproject/controllers/TheController.java
@@ -4,9 +4,6 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.*;
-import test.springtemplateproject.entities.Evenement;
-import test.springtemplateproject.entities.Participant;
-import test.springtemplateproject.repositories.ParticipantRepository;
 import test.springtemplateproject.services.IMyServices;
 
 import java.util.List;
