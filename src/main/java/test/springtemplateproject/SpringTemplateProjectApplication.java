584c44fe4131a44379de069b673b2c3928d5ffa8
radhouene101 <radhouene.berrzig@esprit.tn>
2024-04-29 15:51:10 +0100

    update

--- src/main/java/test/springtemplateproject/SpringTemplateProjectApplication.java
+++ src/main/java/test/springtemplateproject/SpringTemplateProjectApplication.java
@@ -1,4 +1,4 @@
-package test.maryem.springtemplateproject;
+package test.springtemplateproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/test/springtemplateproject/SpringTemplateProjectApplicationTests.java
+++ src/test/java/test/springtemplateproject/SpringTemplateProjectApplicationTests.java
@@ -1,4 +1,4 @@
-package test.maryem.springtemplateproject;
+package test.springtemplateproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
