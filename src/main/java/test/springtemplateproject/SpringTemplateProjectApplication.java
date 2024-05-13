c04d561ca216feabb7868f9abd3016c6ff22c8b7
radhouene101 <radhouene.berrzig@esprit.tn>
2024-05-13 20:16:37 +0100

    prj

--- src/main/java/test/springtemplateproject/SpringTemplateProjectApplication.java
+++ src/main/java/test/springtemplateproject/SpringTemplateProjectApplication.java
@@ -2,8 +2,12 @@ package test.springtemplateproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAspectJAutoProxy
 public class SpringTemplateProjectApplication {
 
     public static void main(String[] args) {
