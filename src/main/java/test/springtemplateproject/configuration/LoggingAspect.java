4c04c6416d4b1ed3d256be97b7d115030fa789fb
radhouene101 <radhouene.berrzig@esprit.tn>
2024-05-13 20:15:11 +0100

    ls

---
+++ src/main/java/test/springtemplateproject/configuration/LoggingAspect.java
@@ -0,0 +1,40 @@
+package test.springtemplateproject.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Aspect
+@Slf4j
+public class LoggingAspect {
+
+
+    @Before("execution(* com.example.stationski.services.*.*(Long,..))")
+    public void logMethodEntry() {
+        log.info("Method executed");
+    }
+
+    @AfterReturning("execution( * com.example.stationski.services.SkieurService.*(..))")
+    public void logMethodExit1(JoinPoint joinPoint) {
+        String name = joinPoint.getSignature().getName();
+        log.info("Out of method without errors : " + name );
+    }
+
+    @AfterThrowing("execution(* com.example.stationski.services.*.*(..))")
+    public void logMethodExit2(JoinPoint joinPoint) {
+        String name = joinPoint.getSignature().getName();
+        log.error("Out of method with erros : " + name );
+    }
+
+    @After("execution(* com.example.stationski.services.*.*(..))")
+    public void logMethodExit(JoinPoint joinPoint) {
+        String name = joinPoint.getSignature().getName();
+        log.info("Out of method : " + name );
+    }
+
+
+}
+
